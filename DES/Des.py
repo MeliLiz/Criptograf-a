9eabb92772b7114ca0b4a9ad9d15daf8e4f3a173
Meli Blancas <melifernandez@ciencias.unam.mx>
2024-09-27 16:14:16 -0600

    Pruebas

--- DES/Des.py
+++ DES/Des.py
@@ -178,6 +178,7 @@ def des_encrypt(text, key):
     # Convertir el texto y la clave en bits
     text = text_to_bits(pad(text))
     key = text_to_bits(key)
+    #print("Texto en bits:", (text))
 
     # Generar subclaves
     subkeys = generate_subkeys(key)
@@ -218,12 +219,56 @@ def hex_to_bits(hex_str):
         bits += [int(bit) for bit in f'{int(char, 16):04b}']
     return bits
 
+weak_keys_hex = [
+    '0101010101010101',
+    'fefefefefefefefe',
+    '1f1f1f1f1f1f1f1',
+    'e0e0e0e0e0e0e0e0'
+]
+
+semi_weak_keys_hex = [
+    '01fe01fe01fe01fe', 'fe01fe01fe01fe01',
+    '1fe01fe01fe01fe0', 'e01fe01fe01fe01f',
+    '01e001e001e001e0', 'e001e001e001e001',
+    '1ffe1ffe1ffe1ffe', 'fe1ffe1ffe1ffe1f',
+    '011f011f011f011f', '1f011f011f011f01',
+    'e0fee0fee0fee0fe', 'fee0fee0fee0fee0'
+]
+
+possible_weak_keys = [
+    '1f1f01010e0e0101', 'e00101e0f10101f1',
+    '011f1f01010e0e01', 'fe1f01e0fe0e01f1',
+    '1f01011f0e01010e', 'fe011fe0fe010ef1',
+    '01011f1f01010e0e', 'e01f1fe0f10e0ef1',
+    'fe0101fefe0101fe',
+    'e0e00101f1f10101', 'e01f01fef10e01fe',
+    'fefe0101fefe0101', 'e0011ffef1010efe',
+    'fee01f01fef10e01', 'fe1f1ffefe0e0efe',
+    'e0fe1f01f1fe0e01',    
+    'fee0011ffef1010e', '1ffe01e00efe01f1',
+    'e0fe011ff1fe010e', '01fe1fe001fe0ef1',
+    'e0e01f1ff1f10e0e', '1fe001fe0ef101fe',
+    'fefe1f1ffefe0e0e', '01e01ffe01f10efe',
+    'fe1fe001fe0ef101', '0101e0e00101f1f1',
+    'e01ffe01f10efe01', '1f1fe0e00e0ef1f1',
+    'fe01e01ffe01f10e', '1f01fee00e0ef1f1',
+    'e001fe1ff101fe0e', '011ffee0010efef1',
+    '1f01e0fe0e01f1fe',
+    '01e0e00101e1e101', '011fe0fe010ef1fe',
+    '1ffee0010efef001', '0101fefe0101fefe',
+    '1ffee0010ef1fe01', '1f1ffefe0e0efefe',
+    '01fefe0101fefe01',    
+    '1fe0e0f10ef1f10e', 'fefee0e0fefef1f1',
+    '01fee01f01fef10e', 'e0fefee0f1fefef1',
+    '01e0fe1f01f1fe0e', 'fee0e0fefef1f1fe',
+    '1ffefe1f0efefe0e', 'e0e0fefef1f1fefe']
+
 
 
 # Texto a cifrar
-plaintext = "Hola Mundo"
+plaintext = "00011100110101"
 # Clave de 8 caracteres (64 bits) para DES
-key = "clave123"
+key = "1f1f1f1f1f1f1f1"
 
 # Cifrar el texto
 ciphertext_bits = des_encrypt(plaintext, key)
@@ -232,5 +277,32 @@ ciphertext_bits = des_encrypt(plaintext, key)
 ciphertext_hex = bits_to_hex(ciphertext_bits)
 
 # Mostrar el texto cifrado en formato hexadecimal
-print("Texto cifrado (hexadecimal):", ciphertext_hex)
+#print("Texto cifrado (hexadecimal):", ciphertext_hex)
+
+
+print("\n 1. Claves débiles")
+for weak_key in weak_keys_hex:
+    ciphertext_bits = des_encrypt(plaintext, weak_key)
+    print("\nKey:", weak_key)
+    print("Texto cifrado:", bits_to_hex(ciphertext_bits))
+    """count = -1
+    for bit in ciphertext_bits:
+        count += 1
+        if count == 8:
+            print(" ", end="")
+            count = 0
+        print(bit, end="")"""
+
+"""print("\n 2. Claves semi-débiles")
+for semi_weak_key in semi_weak_keys_hex:
+    ciphertext_bits = des_encrypt(plaintext, semi_weak_key)
+    print("Key:", semi_weak_key)
+    print("Texto cifrado:", bits_to_hex(ciphertext_bits))
+
+print("\n 3. Posibles claves débiles")
+for possible_weak_key in possible_weak_keys:
+    ciphertext_bits = des_encrypt(plaintext, possible_weak_key)
+    print("Key:", possible_weak_key)
+    print("Texto cifrado:", bits_to_hex(ciphertext_bits))"""
+
 
