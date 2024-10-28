0bb23fd681a4df1a9e86d084598f28bddb736c4f
Meli Blancas <melifernandez@ciencias.unam.mx>
2024-10-28 16:13:00 -0600

    print bits

--- DES/Des_des.py
+++ DES/Des_des.py
@@ -1,36 +1,31 @@
-INITIAL_PERMUTATION = [58, 50, 42, 34, 26, 18, 10, 2,60, 52, 44, 36, 28, 20, 12, 4,
-      62, 54, 46,38, 30, 22, 14, 6, 64, 56, 48, 40, 32, 24, 16, 8,
-      57, 49,41, 33, 25, 17, 9, 1,59, 51, 43, 35, 27, 19, 11, 3,
-      61, 53, 45, 37, 29, 21, 13, 5,63, 55, 47, 39, 31, 23, 15,7]
+
+##################################### Constants #####################################
+INITIAL_PERMUTATION =   [58, 50, 42, 34, 26, 18, 10, 2,60, 52, 44, 36, 28, 20, 12, 4,
+                        62, 54, 46,38, 30, 22, 14, 6, 64, 56, 48, 40, 32, 24, 16, 8,
+                        57, 49,41, 33, 25, 17, 9, 1,59, 51, 43, 35, 27, 19, 11, 3,
+                        61, 53, 45, 37, 29, 21, 13, 5,63, 55, 47, 39, 31, 23, 15,7]
 
 FINAL_PERMUTATION = [40, 8, 48, 16, 56, 24, 64, 32, 39, 7,47, 15, 55, 23, 63, 31,
-      38, 6, 46,14, 54, 22, 62, 30, 37, 5, 45, 13, 53, 21, 61, 29,
-      36, 4, 44, 12,52, 20, 60, 28,35, 3, 43, 11, 51, 19, 59, 27,
-      34, 2, 42, 10, 50, 18, 58, 26, 33, 1, 41, 9, 49, 17, 57, 25]
+                    38, 6, 46,14, 54, 22, 62, 30, 37, 5, 45, 13, 53, 21, 61, 29,
+                    36, 4, 44, 12,52, 20, 60, 28,35, 3, 43, 11, 51, 19, 59, 27,
+                    34, 2, 42, 10, 50, 18, 58, 26, 33, 1, 41, 9, 49, 17, 57, 25]
 
-EXPANSION = [32, 1, 2, 3, 4, 5, 4, 5,6, 7, 8, 9, 8, 9, 10, 11,
-     12, 13, 12, 13, 14, 15, 16, 17, 16, 17, 18, 19, 20,21, 20, 21,
-     22, 23, 24, 25, 24, 25,26, 27, 28, 29, 28, 29, 30, 31, 32, 1]
+P_PERMUTATION_TABLE =   [16, 7, 20, 21, 29, 12, 28, 17,
+                        1, 15, 23, 26, 5, 18,31, 10,
+                        2, 8, 24,14, 32, 27, 3, 9,
+                        19, 13, 30, 6, 22, 11,4, 25]
 
-P_PERMUTATION_TABLE = [16, 7, 20, 21, 29, 12, 28, 17,
-     1, 15, 23, 26, 5, 18,31, 10,
-     2, 8, 24,14, 32, 27, 3, 9,
-     19, 13, 30, 6, 22, 11,4, 25]
-
-#PC1 Permutation table
 PC1_PERMUTATION_TABLE = [57, 49, 41, 33,25, 17, 9, 1, 58, 50, 42, 34, 26, 18,
-       10, 2, 59, 51, 43, 35, 27, 19, 11, 3, 60, 52, 44, 36,
-       63, 55, 47, 39, 31, 23, 15,7, 62, 54, 46, 38, 30, 22,
-       14, 6, 61, 53, 45, 37, 29,21, 13, 5, 28, 20, 12, 4]
+                        10, 2, 59, 51, 43, 35, 27, 19, 11, 3, 60, 52, 44, 36,
+                        63, 55, 47, 39, 31, 23, 15,7, 62, 54, 46, 38, 30, 22,
+                        14, 6, 61, 53, 45, 37, 29,21, 13, 5, 28, 20, 12, 4]
 
-#PC2 Permutation table
 PC2_PERMUTATION_TABLE = [14, 17, 11, 24, 1, 5,3, 28, 15, 6, 21, 10,
-       23, 19, 12, 4, 26, 8,16, 7, 27, 20, 13, 2,
-       41, 52, 31, 37, 47, 55,30, 40, 51, 45, 33, 48,
-       44, 49, 39, 56, 34, 53,46, 42, 50, 36, 29, 32]
+                        23, 19, 12, 4, 26, 8,16, 7, 27, 20, 13, 2,
+                        41, 52, 31, 37, 47, 55,30, 40, 51, 45, 33, 48,
+                        44, 49, 39, 56, 34, 53,46, 42, 50, 36, 29, 32]
 
-#Rotations per round
-ROTATIONS = [1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1]
+ROTATIONS = [1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1] #Rotations per round
 
 S_BOXES = [ [[14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7],[0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8],
             [4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0],[15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13]],
@@ -55,6 +50,13 @@ S_BOXES = [ [[14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7],[0, 15, 7, 4
             
             [[13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7],[1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2],
             [7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8],[2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11]]]
+
+EXPANSION = [32, 1, 2, 3, 4, 5, 4, 5,6, 7, 8, 9, 8, 9, 10, 11,
+            12, 13, 12, 13, 14, 15, 16, 17, 16, 17, 18, 19, 20,21, 20, 21,
+            22, 23, 24, 25, 24, 25,26, 27, 28, 29, 28, 29, 30, 31, 32, 1]
+
+
+##################################### Functions for DES #####################################
 
 # Function to permutate a block of bits according to the table
 def permutation(block, table):
@@ -113,6 +115,9 @@ def get_subkeys(key):
         subkeys.append(permuted)
     return subkeys
 
+
+##################################### String, bits and hex related #####################################
+
 #Padding for the text to be multiple of 64 bits
 def pad(text):
     length = len(text) % 8 #8 bytes = 64 bits
@@ -138,57 +143,9 @@ def bits_to_text(bits):
     chars = []
     for b in range(0, len(bits), 8):
         byte = bits[b:b + 8]
-        chars.append(chr(int(''.join([str(bit) for bit in byte]), 2)))
+        bits1 = ''.join([str(bit) for bit in byte])
+        chars.append(chr(int(bits1, 2)))
     return ''.join(chars)
-
-def des_encrypt(text, key):
-    text = text_to_bits(pad(text))
-    key = text_to_bits(key)
-    #print("Texto en bits:", (text))
-
-    subkeys = get_subkeys(key)
-
-    #Cipher in 64-bit blocks
-    encrypted_bits = []
-    for i in range(0, len(text), 64):
-        block = text[i:i + 64]
-        encrypted_bits += des_encrypt_block(block, subkeys)
-
-    return encrypted_bits
-
-#Encrypt a 64-bit block (16 rounds)
-def des_encrypt_block(block, keys):
-    permuted_block = permutation(block, INITIAL_PERMUTATION)
-    l_half, r_half = permuted_block[:32], permuted_block[32:]
-
-    for i in range(16):
-        l_half, r_half = feistel_round(l_half, r_half, keys[i])
-
-    final_block = permutation(r_half + l_half, FINAL_PERMUTATION)
-    return final_block
-
-def des_decrypt(bits, key):
-    key = text_to_bits(key)
-    subkeys = get_subkeys(key)
-
-    # Descifrado en bloques de 64 bits
-    decrypted_bits = []
-    for i in range(0, len(bits), 64):
-        block = bits[i:i + 64]
-        decrypted_bits += des_decrypt_block(block, subkeys)
-    return bits_to_text(decrypted_bits)
-
-#Decryption of a 64-bit block (16 rounds)
-def des_decrypt_block(block, keys):
-    permuted_block = permutation(block, INITIAL_PERMUTATION)
-    l_half, r_half = permuted_block[:32], permuted_block[32:]
-
-    for i in range(15, -1, -1):
-        l_half, r_half = feistel_round(l_half, r_half, keys[i])
-
-    final_block = permutation(r_half + l_half, FINAL_PERMUTATION)
-    return final_block
-
 
 #To get an hexadecimal representation instead of weird characters
 def bits_to_hex(bits):
@@ -204,16 +161,77 @@ def hex_to_bits(hex_str):
         bits += [int(bit) for bit in f'{int(char, 16):04b}']
     return bits
 
+##################################### DES #####################################
+
+############## Encryption ##############
+#Encrypt a 64-bit block (16 rounds)
+def encrypt_block(block, keys):
+    permuted_block = permutation(block, INITIAL_PERMUTATION)
+    left = permuted_block[:32]
+    right = permuted_block[32:]
+
+    for i in range(16):
+        left, right = feistel_round(left, right, keys[i])
+
+    final_block = permutation(right + left, FINAL_PERMUTATION)
+    return final_block
+
+def des_encrypt(text, key):
+    text = text_to_bits(pad(text))
+    key = text_to_bits(key)
+    #print("Texto en bits:", (text))
+
+    subkeys = get_subkeys(key)
+
+    #Cipher in 64-bit blocks
+    encrypted_bits = []
+    for i in range(0, len(text), 64):
+        block = text[i:i + 64]
+        encrypted_bits += encrypt_block(block, subkeys)
+    return encrypted_bits
+
+############## Decryption ##############
+
+#Decryption of a 64-bit block (16 rounds)
+def decrypt_block(block, keys):
+    permuted_block = permutation(block, INITIAL_PERMUTATION)
+    left, right = permuted_block[:32], permuted_block[32:]
+
+    for i in range(15, -1, -1):
+        left, right = feistel_round(left, right, keys[i])
+
+    final_block = permutation(right + left, FINAL_PERMUTATION)
+    return final_block
+
+def des_decrypt(bits, key):
+    key = text_to_bits(key)
+    subkeys = get_subkeys(key)
+
+    # Descifrado en bloques de 64 bits
+    decrypted_bits = []
+    for i in range(0, len(bits), 64):
+        block = bits[i:i + 64]
+        decrypted_bits += decrypt_block(block, subkeys)
+    return bits_to_text(decrypted_bits)
 
 
-plaintext = "Mi gato es Roni" #Text to encrypt
-key = "llavesct" #8 characters key
 
-ciphertext_bits = des_encrypt(plaintext, key)
-print("Ciphertext bits:", ciphertext_bits)
 
-ciphertext_hex = bits_to_hex(ciphertext_bits)
-print("Texto cifrado (hexadecimal):", ciphertext_hex)
+if __name__ == '__main__':
+    plaintext = "Mi gato es Roni" #Text to encrypt
+    key = "llavesct" #8 characters key
 
-decrypted_text = des_decrypt(ciphertext_bits, key)
-print("Texto descifrado:", decrypted_text)
+    #Encrypt
+    ciphertext_bits = des_encrypt(plaintext, key)
+    print("Ciphertext bits:")
+    for bit in ciphertext_bits:
+        print(bit, end="")
+    print()
+
+    #Get the hexadecimal representation
+    ciphertext_hex = bits_to_hex(ciphertext_bits)
+    print("Texto cifrado (hexadecimal):", ciphertext_hex)
+
+    #DEcrypt the bits gotten from the encryption
+    decrypted_text = des_decrypt(ciphertext_bits, key)
+    print("Texto descifrado:", decrypted_text)
